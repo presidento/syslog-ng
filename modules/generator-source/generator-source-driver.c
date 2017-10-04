6914ab9428b838545d9492e36fc010ff292b17c5
Máté Farkas <mate.farkas@balabit.com>
2017-10-04 21:31:05 +0200

    WIP! add driver init/deinit/free functions
    Signed-off-by: Máté Farkas <mate.farkas@balabit.com>

--- modules/generator-source/generator-source-driver.c
+++ modules/generator-source/generator-source-driver.c
@@ -25,6 +25,58 @@
 #include <stdlib.h>
 #include <glib.h>
 
+static void
+_generator_source_driver_free(LogPipe *s)
+{
+  GeneratorSourceDriver *self = (GeneratorSourceDriver*) s;
+
+  generator_source_options_free(self->options);
+  log_src_driver_free(s);
+}
+
+static gboolean
+_generator_source_driver_deinit(LogPipe *s)
+{
+  GeneratorSourceDriver *self = (GeneratorSourceDriver*) s;
+
+  if(self->source)
+    {
+      log_pipe_deinit((LogPipe *)self->source);
+      log_pipe_unref((LogPipe *)self->source);
+      self->source = NULL;
+    }
+
+  if (!log_src_driver_deinit_method(s))
+    return FALSE;
+
+  return TRUE;
+}
+
+static gboolean
+_generator_source_driver_init(LogPipe *s)
+{
+  GeneratorSourceDriver *self = (GeneratorSourceDriver*) s;
+  GlobalConfig *cfg = log_pipe_get_config(s);
+
+  if (!log_src_driver_init_method(s))
+    return FALSE;
+
+  generator_source_options_init(self->options, cfg, self->super.super.group);
+  /*
+  self->source = generator_source_new(self, self->options);
+  log_pipe_append(&self->source->super, s);
+
+  if (!log_pipe_init(&self->source->super))
+    {
+      log_pipe_unref(&self->source->super);
+      self->source = NULL;
+      return FALSE;
+    }
+  */
+  return TRUE;
+}
+
+
 LogDriver *
 generator_source_driver_new(GlobalConfig *cfg)
 {
@@ -32,13 +84,12 @@ generator_source_driver_new(GlobalConfig *cfg)
 
   log_src_driver_init_instance(&self->super, cfg);
 
-  /* self->super.super.super.init = _generator_source_driver_init; */
-  /* self->super.super.super.deinit = _generator_source_driver_deinit; */
-  /* self->super.super.super.free_fn = _generator_source_driver_free; */
+  self->super.super.super.init = _generator_source_driver_init;
+  self->super.super.super.deinit = _generator_source_driver_deinit;
+  self->super.super.super.free_fn = _generator_source_driver_free;
 
   self->options = generator_source_options_new();
   log_source_options_defaults(&self->options->super);
 
   return &self->super.super;
 }
-
